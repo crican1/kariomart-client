6918ff06de60569888c059bd5f83793f6aef46bb
Mara Caoile <mara.caoile@gmail.com>
2023-08-08 21:22:48 -0500

    refactored race card component and added edit and delete buttons, deleted select cup js

--- components/MapCard.js
+++ components/MapCard.js
@@ -1,9 +1,7 @@
 import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
-import Link from 'next/link';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-// import Link from 'next/link';
 import { RaceContext } from '../utils/context/raceContext';
 
 export default function DisplayMapCard({ mapObj }) {
@@ -14,17 +12,15 @@ export default function DisplayMapCard({ mapObj }) {
       <h2>{mapObj.name}</h2>
       <h4>{mapObj.theme}</h4>
       <h4>{mapObj.difficulty}</h4>
-      <Link passHref href="/selectCup">
-        <Button
-          variant="success"
-          className="m-2"
-          onClick={() => {
-            setSelectedMap(mapObj.id);
-            addToRace({ map_id: mapObj.id });
-          }}
-        >Select!
-        </Button>
-      </Link>
+      <Button
+        variant="success"
+        className="m-2"
+        onClick={() => {
+          setSelectedMap(mapObj.id);
+          addToRace({ map_id: mapObj.id });
+        }}
+      >Select!
+      </Button>
     </Card>
   );
 }
